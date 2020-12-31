c9ff658b9205fef396d57858269251faa1fbffda
Nathan Peek <nmp14fsu@gmail.com>
2020-12-30 21:59:24 -0500

    Add variable for password prompt/confirms

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function writePassword() {
 
 }
 
+function generatePassword() {
+  let passLength = prompt("Choose password length (8 to 128 characters)");
+  let lower = confirm("Do you want lowercase letters?");
+  let upper = confirm("Do you want uppercase letters?");
+  let num = confirm("Do you want numbers?");
+  let specialChar = confirm("Do you want to include special characters?");
+
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
